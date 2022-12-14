1bee3e02265e789d6061ab6ef8ff8b3af9e9e05d
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-12-14 17:48:51 +0000

    Add license header and clean up imports to init

--- pyxivapi/__init__.py
+++ pyxivapi/__init__.py
@@ -1,21 +1,32 @@
-__title__ = 'pyxivapi'
-__author__ = 'Lethys'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2019 (c) Lethys'
-__version__ = '0.5.1'
+"""
+MIT License
 
-from .client import XIVAPIClient
-from .exceptions import (
-    XIVAPIForbidden,
-    XIVAPIBadRequest,
-    XIVAPINotFound,
-    XIVAPIServiceUnavailable,
-    XIVAPIInvalidLanguage,
-    XIVAPIInvalidIndex,
-    XIVAPIInvalidColumns,
-    XIVAPIInvalidFilter,
-    XIVAPIInvalidWorlds,
-    XIVAPIInvalidDatacenter,
-    XIVAPIError,
-    XIVAPIInvalidAlgo
-)
+Copyright (c) 2019 Lethys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__title__ = "pyxivapi"
+__author__ = "Lethys"
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 (c) Lethys"
+__version__ = "0.5.1"
+
+from .client import *
+from .exceptions import *
