dd322ec49eb8388ad55ac00b332a5120b14227e8
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-12-14 17:50:29 +0000

    add license header and cleanup types around models

--- pyxivapi/models.py
+++ pyxivapi/models.py
@@ -1,3 +1,30 @@
+"""
+MIT License
+
+Copyright (c) 2019 Lethys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from typing import ClassVar, List
+
 from .exceptions import XIVAPIInvalidFilter
 
 
@@ -6,17 +33,17 @@ class Filter:
     Model class for DQL filters
     """
 
-    comparisons = ["gt", "gte", "lt", "lte"]
+    comparisons: ClassVar[List[str]] = ["gt", "gte", "lt", "lte"]
 
-    def __init__(self, field: str, comparison: str, value: int):
+    def __init__(self, field: str, comparison: str, value: int) -> None:
         comparison = comparison.lower()
 
         if comparison not in self.comparisons:
             raise XIVAPIInvalidFilter(f'"{comparison}" is not a valid DQL filter comparison.')
 
-        self.Field = field
-        self.Comparison = comparison
-        self.Value = value
+        self.field = field
+        self.comparison = comparison
+        self.value = value
 
 
 class Sort:
@@ -24,6 +51,6 @@ class Sort:
     Model class for sort field
     """
 
-    def __init__(self, field: str, ascending: bool):
-        self.Field = field
-        self.Ascending = ascending
+    def __init__(self, field: str, ascending: bool) -> None:
+        self.field = field
+        self.ascending = ascending
