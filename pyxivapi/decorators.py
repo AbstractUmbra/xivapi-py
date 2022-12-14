5ac2c31ceab62d0deb52dc81e9c5930118a3bd5b
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-12-14 17:49:36 +0000

    add license header, type decorator and cleanup around decorators

--- pyxivapi/decorators.py
+++ pyxivapi/decorators.py
@@ -1,16 +1,57 @@
+"""
+MIT License
+
+Copyright (c) 2019 Lethys
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from __future__ import annotations
+
 import logging
 from functools import wraps
 from time import time
+from typing import TYPE_CHECKING, Any, Callable, Coroutine, TypeVar
 
-__log__ = logging.getLogger(__name__)
+
+if TYPE_CHECKING:
+    from typing_extensions import ParamSpec
+
+    P = ParamSpec("P")
+else:
+    P = TypeVar("P")
+
+C = TypeVar("C")
+T = TypeVar("T")
+
+LOGGER = logging.getLogger(__name__)
 
 
-def timed(func):
+def timed(func: Callable[P, Coroutine[Any, Any, T]]) -> Callable[P, Coroutine[Any, Any, T]]:
     """This decorator prints the execution time for the decorated function."""
+
     @wraps(func)
-    async def wrapper(*args, **kwargs):
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         start = time()
         result = await func(*args, **kwargs)
-        __log__.info("{} executed in {}s".format(func.__name__, round(time() - start, 2)))
+        LOGGER.info("{} executed in {}s".format(func.__name__, round(time() - start, 2)))
         return result
+
     return wrapper
